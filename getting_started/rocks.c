ccf619bfebbb0fef9b9ab655af9820aee9c35ed7
vanstinger <vandykfernandes@hotmail.com>
2016-07-14 01:34:56 +0400

    Initial commit - Back to C Programming...

---
+++ getting_started/rocks.c
@@ -0,0 +1,10 @@
+/* Basic program to print code snippet in C */
+# include <stdio.h>
+
+/* C Program starts with a Main function */
+int main()
+{
+	/* Code goes here..*/
+	puts("Head First C!..");
+	return 0;
+}
